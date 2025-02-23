c1f2ec96ef381888e624a25cfc4fe6175035ad88
Khasan <57419893+gudzilla@users.noreply.github.com>
2025-02-23 22:02:04 +0500

    Added Footer Icons for TS and Redux

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,18 +1,22 @@
-import styles from "./Footer.module.css";
-import ReactIcon from "../../assets/icons/react.svg?react";
-import GithubIcon from "../../assets/icons/github-icon.svg?react";
+import styles from './Footer.module.css';
+import ReactIcon from '../../assets/icons/react.svg?react';
+import GithubIcon from '../../assets/icons/github-icon.svg?react';
+import TsIcon from '../../assets/icons/typescript-icon.svg?react';
+import ReduxIcon from '../../assets/icons/redux-icon.svg?react';
 
 export function Footer() {
   return (
     <footer className={styles.info}>
       <p>
-        Created by{" "}
-        <a href="https://github.com/gudzilla" className={styles.gitLink}>
+        Created by{' '}
+        <a href='https://github.com/gudzilla' className={styles.gitLink}>
           gudzilla <GithubIcon className={styles.gitIcon} />
         </a>
       </p>
       <p className={styles.reactP}>
-        Powered by React <ReactIcon className={styles.reactIcon} />
+        <b>Powered by: </b>React <ReactIcon className={styles.reactIcon} /> TypeScript{' '}
+        <TsIcon className={styles.reactIcon} /> Redux
+        <ReduxIcon className={styles.reactIcon} />
       </p>
     </footer>
   );
